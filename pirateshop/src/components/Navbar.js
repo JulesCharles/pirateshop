481fa04381a50c83c16708dfa0e7c61cf3467938
JulesCharles <julees.charles@gmail.com>
2022-03-16 00:32:01 +0100

    modification Navbar, ajout logo

--- pirateshop/src/components/Navbar.js
+++ pirateshop/src/components/Navbar.js
@@ -1,7 +1,13 @@
 import './styles/Navbar.css'
+import pirateicone from '../assets/pirateicone.jpeg'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='navbar'>
+      <div className='title'>PirateShop</div>
+      <img className='logo' src={pirateicone} width='120px' alt='logo' />
+    </div>
+  )
 }
 
 export default Navbar
