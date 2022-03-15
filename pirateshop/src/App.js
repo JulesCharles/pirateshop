807503f50df93ecedee636703214605201255dbb
JulesCharles <julees.charles@gmail.com>
2022-03-15 23:46:44 +0100

    premier commit, création de Navbar.js

---
+++ pirateshop/src/App.js
@@ -0,0 +1,14 @@
+// import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
